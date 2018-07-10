3bf154438e343cb59820352c75bc13c34c284ca7
yamini priya <yaminipriyakodeboyina@gmail.com>
2018-07-10 14:05:29 +0530

    second commit duplicate

--- getRandomValue.js
+++ getRandomValue.js
@@ -9,7 +9,7 @@
  * @returns {number}
  */
 const getRandomValue = (min, max) => {
-  const x = Math.floor((Math.random() * (max - min + 2)) + min - 1);
+  const x = Math.floor((Math.random() * (max - min + 2)) + min);
   return x;
 };
 
